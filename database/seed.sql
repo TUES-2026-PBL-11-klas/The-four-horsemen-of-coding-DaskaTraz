09a41f75c190c8bc8fe24a8ee8b181587124b0fb
IgnatStoychev <ignat.v.stoychev.2022@elsys-bg.org>
2026-04-19 17:55:48 +0300

    MOREEE CLASSES AND SUBJECTS

--- database/seed.sql
+++ database/seed.sql
@@ -1,10 +1,15 @@
 INSERT INTO classes (name, graduation_year)
 VALUES 
-('10A', 2025),
-('10B', 2025),
-('11A', 2026),
-('11B', 2026),
-('12A', 2027);
+('8A', 2030), ('8B', 2030), ('8C', 2030), ('8D', 2030), ('8E', 2030), ('8F', 2030), ('8G', 2030),
+
+('9A', 2029), ('9B', 2029), ('9C', 2029), ('9D', 2029), ('9E', 2029), ('9F', 2029), ('9G', 2029),
+
+('10A', 2028), ('10B', 2028), ('10C', 2028), ('10D', 2028), ('10E', 2028), ('10F', 2028), ('10G', 2028),
+
+('11A', 2027), ('11B', 2027), ('11C', 2027), ('11D', 2027), ('11E', 2027), ('11F', 2027), ('11G', 2027),
+
+('12A', 2026), ('12B', 2026), ('12C', 2026), ('12D', 2026), ('12E', 2026), ('12F', 2026), ('12G', 2026);
+
 
 INSERT INTO subjects (name)
 VALUES 
@@ -15,4 +20,16 @@ VALUES
 ('History'),
 ('Geography'),
 ('English'),
-('Informatics');
+('German'),
+('French'),
+('Spanish'),
+('Philosophy'),
+('Psychology'),
+('Civics'),
+('Informatics'),
+('Programming'),
+('Art'),
+('Music'),
+('Physical Education'),
+('Economics'),
+('Entrepreneurship');
