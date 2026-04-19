413788c65a550ff7566fa7d55a7ab609f607e8b5
IgnatStoychev <ignat.v.stoychev.2022@elsys-bg.org>
2026-04-19 17:48:42 +0300

    remove inserts, add subjects and classes

--- database/seed.sql
+++ database/seed.sql
@@ -1,22 +1,18 @@
-INSERT INTO users (username, email, password_hash, role)
-VALUES
-('student1', 's1@mail.com', 'hash', 'STUDENT'),
-('teacher1', 't1@mail.com', 'hash', 'TEACHER');
-
 INSERT INTO classes (name, graduation_year)
-VALUES ('11A', 2026);
-
-INSERT INTO students (user_id, class_id, number_in_class)
-VALUES (1, 1, 1);
-
-INSERT INTO teachers (user_id)
-VALUES (2);
+VALUES 
+('10A', 2025),
+('10B', 2025),
+('11A', 2026),
+('11B', 2026),
+('12A', 2027);
 
 INSERT INTO subjects (name)
-VALUES ('Math');
-
-INSERT INTO teachers_subjects (teacher_id, subject_id, class_id)
-VALUES (1, 1, 1);
-
-INSERT INTO grades (value, student_id, subject_id, teacher_id)
-VALUES (5.50, 1, 1, 1);
+VALUES 
+('Math'),
+('Physics'),
+('Chemistry'),
+('Biology'),
+('History'),
+('Geography'),
+('English'),
+('Informatics');
