67191bfd2987290eea07f76086d6203285c7c10e
kaloian3141 <kaloian70308@gmail.com>
2026-04-24 14:58:54 +0300

    pipelinetest

--- vue-frontend/src/api/axios.js
+++ vue-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import router from '@/router/index.js'
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "/api",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
